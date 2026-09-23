a472d4437885060bc0eaf1838c9149e5242948cb
Florious95 <281215401+Florious95@users.noreply.github.com>
2026-09-23 12:41:01 +0800

    test(nodeprobe): correct Linux manifest source commit

--- internal/nodeprobe/manifest_test.go
+++ internal/nodeprobe/manifest_test.go
@@ -21,7 +21,7 @@ func TestAcceptedManifestPinsNSourceAndArtifacts(t *testing.T) {
 	wantExtensionSourceCommit := "ccf8324ebe29e35038412ffad03157e269029232"
 	wantExtensionSourceTree := "6c5e782393741fe229216320fd14e2b5d1100231"
 	if wantPlatform == "linux/amd64" {
-		wantCommit = "a7185aa2b93440ac9fd289c0de63d7767f8ef749"
+		wantCommit = "a7185aa2b93460ac9fd289c0de63d7767f8ef749"
 		wantTree = "28e3aab4fac89374c0ac4b8c80ba7f13aaae2c75"
 		wantBinaryHash = "61d6dd99e7d7135e20b885b00724fb1359c1643c634857702239a092faa4e958"
 		wantBinarySize = 1108816
